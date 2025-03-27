a28102a8016e07adb0afb910b272099d58a2fd58
littlesekii <contact.sekii@gmail.com>
2025-03-27 20:08:40 -0300

    added text in the another view

--- src/views/AnotherView.vue
+++ src/views/AnotherView.vue
@@ -1,7 +1,15 @@
 <template>
-  
+    <main>
+        <h1>Another View</h1>
+    </main>
 </template>
 
-<style>
-
+<style scoped>
+    main {
+        height: 100vh;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;        
+    }
 </style>
