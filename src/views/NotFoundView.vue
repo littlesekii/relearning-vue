26854efc7b28684160a161eed0b40ef22e5c6159
littlesekii <contact.sekii@gmail.com>
2025-03-27 19:52:33 -0300

    project started

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        <h1>404 | Not Found</h1>
+    </main>
+</template>
+
+<style scoped>
+    main {
+        height: 100vh;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;        
+    }
+</style>
