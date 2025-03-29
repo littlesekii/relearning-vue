79f466fc71c77fa0541ce6f268d8de59b9fba49d
littlesekii <contact.sekii@gmail.com>
2025-03-28 21:48:29 -0300

    feat.: added DirectiveVFor component examples

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import DirectiveVFor from '@/components/Directives/DirectiveVFor.vue';
 import LifecycleHooks from '@/components/LifecycleHooks.vue';
 import Reactivity from '@/components/Reactivity.vue';
 </script>
 
 <template>
   <!-- <LifecycleHooks /> -->
-  <Reactivity />
+  <!-- <Reactivity /> -->
+  <DirectiveVFor />
 </template>
